bbad9848f66964e41e928b427afc873b404dfde3
anicca <meresan02david1006@gmail.com>
2022-10-09 12:17:15 +0300

    ExpressionParser sketch

---
+++ src/ExpressionParser.java
@@ -0,0 +1,15 @@
+public class ExpressionParser {
+    String[] args;
+
+    public ExpressionParser(String[] args) {
+        this.args = args;
+    }
+
+    private boolean valid(){
+        return false;
+    }
+
+    public Complex result(){
+        return null;
+    }
+}
